bdc993bdfd72b3cd778fb69813eb75ca2c1379b4
Joan Code <bennetjoanwegener@gmail.com>
2025-07-21 19:48:58 +0200

    fixing bugs

--- src/main/java/de/joancode/crate/item/CrateKeyItem.java
+++ src/main/java/de/joancode/crate/item/CrateKeyItem.java
@@ -1,9 +1,15 @@
 package de.joancode.crate.item;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 
 public class CrateKeyItem extends Item {
     public CrateKeyItem(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public boolean isFoil(ItemStack stack) {
+        return true;
     }
 }
